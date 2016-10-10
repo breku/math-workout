8f1274748d3a391a7cbe59910c9ed4a30e84d3ca
brekol <brekol@gmail.com>
2016-10-10 19:44:40 +0200

    init commit

---
+++ desktop/src/com/breku/math/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.breku.math.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.breku.math.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
