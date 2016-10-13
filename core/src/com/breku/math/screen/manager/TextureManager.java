b71b76a07233fc73220d8d89419a5f25020a9c7d
brekol <brekol@gmail.com>
2016-10-13 22:10:52 +0200

    logging loading assets progress

--- core/src/com/breku/math/screen/manager/TextureManager.java
+++ core/src/com/breku/math/screen/manager/TextureManager.java
@@ -1,16 +1,21 @@
 package com.breku.math.screen.manager;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.TextureLoader;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.Texture;
+
+import java.util.Locale;
 
 /**
  * Created by brekol on 13.10.16.
  */
 public class TextureManager {
 
+    private static final String TAG = "TextureManager";
     private AssetManager assetManager;
+    private boolean finishedLoading = false;
 
     public TextureManager() {
         initialize();
@@ -18,7 +23,7 @@ public class TextureManager {
 
     private void initialize() {
         assetManager = new AssetManager();
-        assetManager.setLoader(Texture.class , new TextureLoader(new InternalFileHandleResolver()));
+        assetManager.setLoader(Texture.class, new TextureLoader(new InternalFileHandleResolver()));
         assetManager.load(TextureType.QUICK_MATCH_BUTTON_TEXTURE.getKey(), Texture.class);
         assetManager.load(TextureType.PLAY_WITH_FRIEND_BUTTON_TEXTURE.getKey(), Texture.class);
         assetManager.load(TextureType.INBOX_BUTTON_TEXTURE.getKey(), Texture.class);
@@ -28,7 +33,12 @@ public class TextureManager {
     }
 
     public boolean update() {
-        return assetManager.update();
+        if (!finishedLoading) {
+            Gdx.app.log(TAG, String.format(Locale.ENGLISH, "Loading assets progress=[%2f]", assetManager.getProgress()));
+            final boolean update = assetManager.update();
+            finishedLoading |= update;
+        }
+        return finishedLoading;
     }
 
     public Texture getTexture(TextureType textureType) {
