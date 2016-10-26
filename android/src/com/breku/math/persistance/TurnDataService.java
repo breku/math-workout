c5c04761592a217d19506f43662dedb841575507
brekol <brekol@gmail.com>
2016-10-26 18:20:32 +0200

    gson turnData

--- android/src/com/breku/math/persistance/TurnDataService.java
+++ android/src/com/breku/math/persistance/TurnDataService.java
@@ -1,10 +1,8 @@
 package com.breku.math.persistance;
 
 import android.util.Log;
-import com.breku.math.game.level.GameType;
-import com.breku.math.game.level.LevelDifficulty;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
@@ -17,6 +15,7 @@ public class TurnDataService {
     private static final String TAG = "TurnDataService";
 
     private TurnData turnData;
+    private Gson gson = new GsonBuilder().create();
 
     public void clearTurnData() {
         turnData = null;
@@ -28,56 +27,20 @@ public class TurnDataService {
 
     // Creates a new instance of TurnDataService.
     public TurnData unpersist(byte[] byteArray) {
-
         final String jsonString = getStringFromByteArray(byteArray);
         Log.d(TAG, String.format("Unpersisting jsonString=[%s]", jsonString));
+        return gson.fromJson(jsonString, TurnData.class);
 
-        TurnData result = new TurnData();
-
-        try {
-            JSONObject obj = new JSONObject(jsonString);
-
-            if (obj.has("data")) {
-                result.setData(obj.getString("data"));
-            }
-            if (obj.has("turnCounter")) {
-                result.setTurnCounter(obj.getInt("turnCounter"));
-            }
-            if (obj.has("levelDifficulty")) {
-                final int levelDifficultyOrdinal = obj.getInt("levelDifficulty");
-                result.setLevelDifficulty(LevelDifficulty.values()[levelDifficultyOrdinal]);
-            }
-            if (obj.has("gameType")) {
-                final int gameTypeOrdinal = obj.getInt("gameType");
-                result.setGameType(GameType.values()[gameTypeOrdinal]);
-            }
-
-        } catch (JSONException e) {
-            Log.e(TAG, "Error during getting data from json. " + e.getMessage(), e);
-
-        }
-
-        return result;
-    }
-
-    public void incrementTurnCounter() {
-        turnData.setTurnCounter(turnData.getTurnCounter() + 1);
     }
 
     // This is the byte array we will write out to the TBMP API.
     public byte[] persist() {
-        final JSONObject jsonObject = new JSONObject();
+        final String jsonString = gson.toJson(turnData);
+        return getBytesFromJsonObject(jsonString);
+    }
 
-        try {
-            jsonObject.put("data", turnData.getData());
-            jsonObject.put("turnCounter", turnData.getTurnCounter());
-            jsonObject.put("levelDifficulty", turnData.getLevelDifficulty().ordinal());
-            jsonObject.put("gameType", turnData.getGameType().ordinal());
-
-        } catch (JSONException e) {
-            Log.e(TAG, "Error during persisting turn data. " + e.getMessage(), e);
-        }
-        return getBytesFromJsonObject(jsonObject);
+    public void incrementTurnCounter() {
+        turnData.setTurnCounter(turnData.getTurnCounter() + 1);
     }
 
     private String getStringFromByteArray(byte[] byteArray) {
@@ -89,8 +52,7 @@ public class TurnDataService {
         }
     }
 
-    private byte[] getBytesFromJsonObject(JSONObject jsonObject) {
-        final String jsonString = jsonObject.toString();
+    private byte[] getBytesFromJsonObject(final String jsonString) {
         Log.d(TAG, String.format("Persisting jsonString=[%s]", jsonString));
         return jsonString.getBytes(Charset.forName("UTF-8"));
     }
